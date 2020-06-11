7b35a3b4d4c19556536b46b6dede2a329abe7fc1
Henry Krzywy <henrykr93@gmail.com>
2020-06-10 23:52:01 -0300

    Implementação das 4 operações na url

--- core/views.py
+++ core/views.py
@@ -5,3 +5,19 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, nome):
     return HttpResponse('<h1>Hello {}</h1>'.format(nome))
+
+
+def soma(request, num1, num2):
+    return HttpResponse('<h1>A soma é {}</h1>'.format(num1 + num2))
+
+
+def subtracao(request, num1, num2):
+    return HttpResponse('<h1>A subtração é {}</h1>'.format(num1 - num2))
+
+
+def multiplicacao(request, num1, num2):
+    return HttpResponse('<h1>A multiplicação é {}</h1>'.format(num1 * num2))
+
+
+def divisao(request, num1, num2):
+    return HttpResponse('<h1>A divisao é {}</h1>'.format(num1 / num2))
